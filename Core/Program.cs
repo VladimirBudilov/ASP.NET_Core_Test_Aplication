388f0a5f74f26ddb214dc0fa0e9e1625302f7ac8
Vladimir <v.ladimirbudilov@yandex.ru>
2024-03-01 16:10:40 +0400

    add cookies

--- Core/Program.cs
+++ Core/Program.cs
@@ -3,15 +3,36 @@ using Core.Services;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IResponseFomatter, HtmlFormatter>(); 
-//builder.Services.AddTransient<IResponseFomatter, TextFormatter>();
-//builder.Services.Add<IResponseFomatter, HtmlFormatter>();
 
 var app = builder.Build();
 
-app.MapGet("/format", async (HttpContext context, IResponseFomatter formatter) =>
+app.MapGet("/cookie", async (context) =>
     {
-       await formatter.FormatResponse(context);
+        if(!int.TryParse(context.Request.Cookies["counter"], out int counter))
+        {
+            counter = 1;
+        }
+        counter++;
+        context.Response.Cookies.Append(
+            "counter",
+            (counter).ToString(),
+            new CookieOptions
+            {
+                MaxAge = TimeSpan.FromMinutes(10)
+            });
+        await context.Response.WriteAsync($"Counter: {counter}");
     });
-app.UseStaticFiles();
+
+app.MapGet("/clear", context =>
+{
+    context.Response.Cookies.Delete("counter");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+});
+
+app.MapGet("/", async (context) =>
+{
+    await context.Response.WriteAsync("Hello World!");
+});
+
 app.Run();
